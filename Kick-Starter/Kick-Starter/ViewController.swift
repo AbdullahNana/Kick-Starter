55acdff5ff0097fda428a2dd1dd2e3ec47a8b496
AbdullahNanaDVT <anana@jhb.dvt.co.za>
2021-10-04 10:05:58 +0200

    Initial Commit

---
+++ Kick-Starter/Kick-Starter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kick-Starter
+//
+//  Created by Abdullah Nana on 2021/10/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
