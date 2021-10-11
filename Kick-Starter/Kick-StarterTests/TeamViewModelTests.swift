abb7bdba9aeaf6f238c61a02a2d8c15a420a06e6
AbdullahNanaDVT <anana@jhb.dvt.co.za>
2021-10-11 09:06:41 +0200

    Created test file

---
+++ Kick-Starter/Kick-StarterTests/TeamViewModelTests.swift
@@ -0,0 +1,18 @@
+//
+//  TeamViewModelTests.swift
+//  Kick-StarterTests
+//
+//  Created by Abdullah Nana on 2021/10/11.
+//
+
+import XCTest
+@testable import Kick_Starter
+
+class TeamViewModelTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+}
