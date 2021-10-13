ad3e4cbf5aa5367c3d54a37e734db912d78d64ec
AbdullahNanaDVT <anana@jhb.dvt.co.za>
2021-10-13 08:01:17 +0200

    Fixed mocks

--- Kick-Starter/Kick-Starter/Display Teams In Leagues/View Models/TeamViewModel.swift
+++ Kick-Starter/Kick-Starter/Display Teams In Leagues/View Models/TeamViewModel.swift
@@ -19,14 +19,12 @@ final class TeamViewModel {
     
     func fetchTeamData() {
         teamRepository.fetchTeamData(method: .GET, endpoint: .teamData) { [weak self] result in
-            DispatchQueue.main.async {
-                switch result {
-                case .success(let team):
-                    self?.teamResponse = team
-                    self?.delegate?.refreshViewContents()
-                case .failure(let error):
-                    self?.delegate?.showErrorMessage(error: error)
-                }
+            switch result {
+            case .success(let team):
+                self?.teamResponse = team
+                self?.delegate?.refreshViewContents()
+            case .failure(let error):
+                self?.delegate?.showErrorMessage(error: error)
             }
         }
     }
--- Kick-Starter/Kick-StarterTests/TeamViewModelTests.swift
+++ Kick-Starter/Kick-StarterTests/TeamViewModelTests.swift
@@ -10,14 +10,34 @@ import XCTest
 
 class TeamViewModelTests: XCTestCase {
     private var mockedTeamRepository: MockTeamRepository!
-    private var mockedTeamViewModel: MockedTeamViewModel!
+    private var viewModelUnderTest: TeamViewModel!
+    private var mockedDelegate: TeamViewModelDelegate!
 
     override func setUp() {
         mockedTeamRepository = MockTeamRepository()
-        mockedTeamViewModel = MockedTeamViewModel(repository: mockedTeamRepository)
+        mockedDelegate = MockDelegate()
+        viewModelUnderTest = TeamViewModel(repository: mockedTeamRepository, delegate: mockedDelegate)
     }
-    func testNumberOfTeams() throws {
-        mockedTeamViewModel.fetchTeamData()
-        XCTAssert(!mockedTeamViewModel.teamData.isEmpty)
+    func testNumberOfTeams() {
+        viewModelUnderTest.fetchTeamData()
+        XCTAssertEqual(viewModelUnderTest.teamData(at: 0)?.team.name, "Manchester United")
+    }
+    class MockDelegate: TeamViewModelDelegate {
+        func refreshViewContents() {
+        }
+        func showErrorMessage(error: Error) {
+        }
+    }
+    final class MockTeamRepository: Repositable {
+        let mockedTeamData = Response(team: Team(id: 33, name: "Manchester United", country: "England",
+                                                 founded: 1878,
+                                                 logo: "https://media.api-sports.io/football/teams/33.png"),
+                                      venue: Venue(id: 557, name: "Old Trafford", city: "Manchester",
+                                                   capacity: 76212,
+                                                   image: "https://media.api-sports.io/football/venues/556.png"))
+        func fetchTeamData(method: HTTPMethod, endpoint: ApiEndpoint,
+                           completionHandler: @escaping TeamRepositoryResultBlock) {
+            completionHandler(.success(SoccerTeamResponseModel(get: "teams", response: [mockedTeamData])))
+        }
     }
 }
