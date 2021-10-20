dc55a2b38591094e8b98bc9c341d014b18db2234
AbdullahNanaDVT <anana@jhb.dvt.co.za>
2021-10-20 22:49:13 +0200

    Added some styling for venue view

--- Kick-Starter/Kick-Starter/Single Venue/Single Venue View Model/SingleVenueViewModel.m
+++ Kick-Starter/Kick-Starter/Single Venue/Single Venue View Model/SingleVenueViewModel.m
@@ -23,7 +23,7 @@
     return _singleVenue.name;
 }
 - (NSString *) venueCapacity {
-    NSString* capacity = [NSString stringWithFormat:@"Venue Capacity: %lii",(long) (long)_singleVenue.capacity];
+    NSString* capacity = [NSString stringWithFormat:@"Venue Capacity: %li",(long) (long)_singleVenue.capacity];
     return capacity;
 }
 - (NSString *) venueCity {
