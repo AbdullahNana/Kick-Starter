2da908937f2365ad837e840f69e861f29fc111ea
AbdullahNanaDVT <anana@jhb.dvt.co.za>
2021-10-22 08:35:33 +0200

    Fixed tests

--- Kick-Starter/Kick-Starter/Display Teams/Models/SoccerTeamResponseModel.swift
+++ Kick-Starter/Kick-Starter/Display Teams/Models/SoccerTeamResponseModel.swift
@@ -22,12 +22,20 @@ struct Team: Codable {
     var capacity: Int?
     let image: String?
     
+    init(id: Int, name: String, city: String, capacity: Int, image: String) {
+        self.id = id
+        self.name = name
+        self.city = city
+        self.capacity = capacity
+        self.image = image
+    }
+    
     var cap: NSNumber? {
         get {
             return capacity as NSNumber?
         }
         set(newNumber) {
-            capacity = newNumber?.intValue ?? 0
+            capacity = newNumber?.intValue
         }
     }
 }
